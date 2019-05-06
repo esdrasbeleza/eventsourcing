78dad2fb204a42024a76e4613ece863030b6dc6d
Esdras Beleza de Noronha <github@contato.esdrasbeleza.com>
2019-05-06 18:50:16 +0100

    Connect to Postgres

--- backend/go.mod
+++ backend/go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/google/uuid v1.1.1
+	github.com/lib/pq v1.1.1
 	github.com/stretchr/testify v1.3.0
 )
---
+++ backend/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func TestMain(m *testing.M) {
+	defer testShutdown()
+	testSetup()
+	code := m.Run()
+	os.Exit(code)
+}
+
+func testSetup() {
+	connectToTestDB()
+	checkConnection()
+}
+
+func testShutdown() {
+	if db != nil {
+		db.Close()
+	}
+}
+
+func connectToTestDB() {
+	connStr := "postgres://postgres:docker@localhost/postgres?sslmode=disable"
+
+	var err error
+	db, err = sql.Open("postgres", connStr)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func checkConnection() {
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+}
