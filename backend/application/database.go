86c7a3a75204991b5a498a0d0ff367d9d4ed381c
Esdras Beleza de Noronha <github@contato.esdrasbeleza.com>
2019-06-17 23:31:59 +0100

    Get PostgreSQL URL from env

--- backend/application/database.go
+++ backend/application/database.go
@@ -2,10 +2,23 @@ package application
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
 	"github.com/esdrasbeleza/eventsourcing/backend/storage"
 )
 
 func DB() *sql.DB {
-	return storage.ConnectToDB("postgres://postgres:docker@localhost/postgres?sslmode=disable")
+	dbHost := os.Getenv("DATABASE_HOST")
+
+	if dbHost == "" {
+		dbHost = "localhost"
+	}
+
+	log.Printf("Database is %s:5432\n", dbHost)
+
+	dbURL := fmt.Sprintf("postgres://postgres:docker@%s/postgres?sslmode=disable", dbHost)
+
+	return storage.ConnectToDB(dbURL)
 }
